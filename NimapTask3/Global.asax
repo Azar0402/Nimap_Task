7f6f96b2c54afef2c8250fc48e6280562fe60c61
Shahaji-Babar <v-shahaji.d@validusfintech.com>
2025-01-20 18:33:29 +0530

    NimapTask3

---
+++ NimapTask3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NimapTask3.MvcApplication" Language="C#" %>
---
+++ NimapTask3/ICategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using NimapTask3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimapTask3
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetCategories();
+        Category GetCategoryById(int id);
+        void AddCategory(Category category);
+        void UpdateCategory(Category category);
+        void DeleteCategory(int id);
+        void Save();
+
+    }
+}
